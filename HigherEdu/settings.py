061dbe1bb6db2b4f2de3b1a507c72bc22d94975c
N.Venkat Swaroop <venkatnvs2005@gmail.com>
2023-11-23 09:51:46 +0530

    Update settings.py

--- HigherEdu/settings.py
+++ HigherEdu/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from decouple import config
 from django.contrib import messages
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,10 +94,10 @@ WSGI_APPLICATION = 'HigherEdu.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('POSTGRESQL_URL'),
+        conn_max_age=600
+    )
 }
 
 
