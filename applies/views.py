44c0e7540e47a42c8fedbb1ea49180c0ed5ace7c
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2023-12-19 00:21:33 +0530

    updated ads-(borders) and courses filters and export

--- applies/views.py
+++ applies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse,get_object_or_404
 from .models import AvaliableCourses,ApplyCourse
 from django.views import View
 from django.http import HttpResponseNotFound
@@ -6,6 +6,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ApplyLanguagesForm,ApplyCoursesForm
 from .models import ForeignLanguageApplied,ForeignLanguage,CompetitiveCourse,CompetitiveCourseApplied
 from django.contrib import messages
+import os
+from django.conf import settings
+import json
 
 def CountryByApply(request):
     crs = AvaliableCourses.objects.filter(is_active=True)
@@ -15,16 +18,69 @@ def CountryByApply(request):
     return render(request,'applies/bycnt/index.html',context)
 
 class ApplyForCourse(LoginRequiredMixin,View):
+    file_path = os.path.join(settings.BASE_DIR,'datasets/countries.json')
+
+    def check_url_id(self,id):
+        av = get_object_or_404(AvaliableCourses,id=id)
+        return av
+
+    def get_cnt(self,c):
+        with open(self.file_path, 'r') as file:
+            data = json.load(file)
+        country = ""
+        for i in data:
+            if c.lower() == i['code'].lower():
+                country = i['name']
+        return country
+    
+    def dispatch(self, request, *args, **kwargs):
+        av = self.check_url_id(self.kwargs.get("id", None))
+        if not request.user.is_authenticated:
+            messages.info(request, "You need to be logged in to apply for this course.")
+            return redirect('accounts-login')
+        if av:
+            acc = ApplyCourse.objects.filter(user=request.user, course=av)
+            if acc.exists():
+                messages.info(request, "Course Already Applied.")
+                return redirect('applies-courses')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self,request,id):
-        av = AvaliableCourses.objects.filter(id=id)
-        if not av.exists():
-            return HttpResponseNotFound("Course not found. Try aganin!")
-        av = av.first()
+        av = self.check_url_id(self.kwargs.get("id", None))
         context = {
-            "av_course":av
+            "av_course":av,
+            "cnt_a":self.get_cnt(av.country_code)
         }
         return render(request, 'applies/bycnt/apply_form.html',context)
+    
+    def post(self,request,id):
+        course = request.POST['course']
+        if not course:
+            messages.error(request,"Course cannot be Empty")
+            return redirect('applies-courses-apply',id=id)
+        preferred_college = request.POST.getlist('preferred_college[]')
+        if len(preferred_college)<=0:
+            messages.error(request,"Preferred colleges cannot be Empty")
+            return redirect('applies-courses-apply',id=id)
+        abroad_year = request.POST['abroad_year']
+        if not abroad_year:
+            messages.error(request,"Year Of Study cannot be Empty")
+            return redirect('applies-courses-apply',id=id)
+        season = request.POST['season']
+        preferred_college_list = '\r\n'.join([college for college in preferred_college])
+        av = self.check_url_id(id)
+        ac = ApplyCourse.objects.create(
+            user = request.user,
+            course = av,
+            preferred_college = preferred_college_list,
+            course_type = course,
+            abroad_year = abroad_year,
+            abroad_season = season,
+            is_active = True
+        )
+        av.save()
+        messages.success(request,f"Applied to Course in {av.country}.")
+        return redirect('applies-courses')
     
 class ApplyForeignLanguagesView(LoginRequiredMixin,View):
     form = ApplyLanguagesForm()
--- ctm_admin/subapps/applies_adm/views.py
+++ ctm_admin/subapps/applies_adm/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render,HttpResponse
 from django.urls import reverse,reverse_lazy
 from django.views.generic import ListView,UpdateView,DetailView,DeleteView,CreateView
 from ctm_admin.mixins import CheckAdminMixin
-from applies.models import ForeignLanguageApplied,CompetitiveCourseApplied
+from applies.models import ForeignLanguageApplied,CompetitiveCourseApplied,ForeignLanguage,CompetitiveCourse
 import xlwt
 import csv
 import datetime
 from django.db.models import F,Value, CharField
 from django.db.models.functions import Cast, Coalesce, Concat
 from ctm_admin.decorators import check_admin_required
+from django.db.models import Q
 
 class AdminForeignLangList(CheckAdminMixin,ListView):
     model = ForeignLanguageApplied
@@ -18,6 +19,20 @@ class AdminForeignLangList(CheckAdminMixin,ListView):
         'applies': ['view_foreignlanguageapplied']
     }
 
+    def get_queryset(self):
+        filter_val = self.request.GET.get('filter','')
+        if filter_val != "":
+            fl_app = ForeignLanguageApplied.objects.filter(Q(languages__name__contains=filter_val)).distinct()
+        else:
+            fl_app = ForeignLanguageApplied.objects.all()
+        return fl_app
+
+    def get_context_data(self, **kwargs):
+        context = super(AdminForeignLangList, self).get_context_data(**kwargs)
+        context['filter']=self.request.GET.get('filter','')
+        context['filter_av']=ForeignLanguage.objects.all()
+        return context
+
 class AdminCompetitiveCoursesList(CheckAdminMixin,ListView):
     model = CompetitiveCourseApplied
     context_object_name = 'courses'
@@ -26,10 +41,25 @@ class AdminCompetitiveCoursesList(CheckAdminMixin,ListView):
         'applies': ['view_competitivecourseapplied']
     }
 
+    def get_queryset(self):
+        filter_val = self.request.GET.get('filter','')
+        if filter_val != "":
+            cc_app = CompetitiveCourseApplied.objects.filter(Q(course__name__contains=filter_val)).distinct()
+        else:
+            cc_app = CompetitiveCourseApplied.objects.all()
+        return cc_app
+
+    def get_context_data(self, **kwargs):
+        context = super(AdminCompetitiveCoursesList, self).get_context_data(**kwargs)
+        context['filter']=self.request.GET.get('filter','')
+        context['filter_av']=CompetitiveCourse.objects.all()
+        return context
+
 # Export Data Users
 
 @check_admin_required
 def ForeignLangUserDataExportExcel(request):
+    filter_val = request.GET.get('filter','')
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename=UserData'+str(datetime.datetime.now())+'.xls'
     wb = xlwt.Workbook(encoding='utf-8')
@@ -41,7 +71,7 @@ def ForeignLangUserDataExportExcel(request):
     for col_num in range(len(columns)):
         ws.write(row_num, col_num, columns[col_num], font_style)
     font_style =  xlwt.XFStyle()
-    rows = ForeignLanguageApplied.objects.filter(user__is_superuser=False).annotate(
+    rows = ForeignLanguageApplied.objects.filter(user__is_superuser=False,languages__name__contains=filter_val).annotate(
         first_name=F('user__first_name'),
         last_name=F('user__last_name'),
         full_name=Coalesce(Concat(Cast('first_name', CharField()), Value(' '), Cast('last_name', CharField())), 'first_name')
@@ -62,13 +92,14 @@ def ForeignLangUserDataExportExcel(request):
 
 @check_admin_required
 def ForeignLangUserDataExportCsv(request):
+    filter_val = request.GET.get('filter','')
     response = HttpResponse(content_type='text/csv')
     file_name = f'UserData{str(datetime.datetime.now())}.csv'
     response['Content-Disposition'] = 'attachment; filename='+file_name
     writer = csv.writer(response)
     writer.writerow(['Email','Full Name','Gender','Mobile No.','Verification Status','Applied List','Created at'])
 
-    rows = ForeignLanguageApplied.objects.filter(user__is_superuser=False).annotate(
+    rows = ForeignLanguageApplied.objects.filter(user__is_superuser=False,languages__name__contains=filter_val).annotate(
         first_name=F('user__first_name'),
         last_name=F('user__last_name'),
         full_name=Coalesce(Concat(Cast('first_name', CharField()), Value(' '), Cast('last_name', CharField())), 'first_name')
@@ -90,6 +121,7 @@ def ForeignLangUserDataExportCsv(request):
 
 @check_admin_required
 def CompetitiveCoursesUserDataExportExcel(request):
+    filter_val = request.GET.get('filter','')
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename=UserData'+str(datetime.datetime.now())+'.xls'
     wb = xlwt.Workbook(encoding='utf-8')
@@ -101,7 +133,7 @@ def CompetitiveCoursesUserDataExportExcel(request):
     for col_num in range(len(columns)):
         ws.write(row_num, col_num, columns[col_num], font_style)
     font_style =  xlwt.XFStyle()
-    rows = CompetitiveCourseApplied.objects.filter(user__is_superuser=False).annotate(
+    rows = CompetitiveCourseApplied.objects.filter(user__is_superuser=False,course__name__contains=filter_val).annotate(
         first_name=F('user__first_name'),
         last_name=F('user__last_name'),
         full_name=Coalesce(Concat(Cast('first_name', CharField()), Value(' '), Cast('last_name', CharField())), 'first_name')
@@ -122,13 +154,14 @@ def CompetitiveCoursesUserDataExportExcel(request):
 
 @check_admin_required
 def CompetitiveCoursesUserDataExportCsv(request):
+    filter_val = request.GET.get('filter','')
     response = HttpResponse(content_type='text/csv')
     file_name = f'UserData{str(datetime.datetime.now())}.csv'
     response['Content-Disposition'] = 'attachment; filename='+file_name
     writer = csv.writer(response)
     writer.writerow(['Email','Full Name','Gender','Mobile No.','Verification Status','Applied List','Created at'])
 
-    rows = CompetitiveCourseApplied.objects.filter(user__is_superuser=False).annotate(
+    rows = CompetitiveCourseApplied.objects.filter(user__is_superuser=False,course__name__contains=filter_val).annotate(
         first_name=F('user__first_name'),
         last_name=F('user__last_name'),
         full_name=Coalesce(Concat(Cast('first_name', CharField()), Value(' '), Cast('last_name', CharField())), 'first_name')
